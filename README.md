31e31372e066bd2ddc35f9a7a7684fdbb3bc4ac2
Sharon <sharonelizalex@gmail.com>
2021-11-23 12:52:32 +1100

    design game class

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # word-guessing-game
 
 Application to play a word guessing game from the command line.
+
+## Acceptance Criteria
+
+* It's done when the `Letter` and `Word` classes fulfill the tests in the `tests` folder.
+
+* It's done when the user can run the command `npm start` to play the game.
--- test/letter.test.js
+++ test/letter.test.js
@@ -3,6 +3,17 @@ const Letter = require("../lib/letter");
 
 describe("Letter class", ()=>{
     it("characters that are not digits or letters are isntantly visible",()=>{
-        expect(new Letter("?").visible).toBe(true)
+        expect(new Letter("@").visible).toBe(true)
+    })
+
+    it("toString returns _ for letters", ()=>{
+        expect (new Letter("a").toString()).toBe("_")
+    })
+
+    it("correct guess returns true", ()=>{
+        expect(new Letter("j").guess("j")).toBe(true)
     })
 })
+
+
+
