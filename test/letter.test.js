85474e4616614b415e96cf1272d6db55d4272d36
Sharon <sharonelizalex@gmail.com>
2021-11-25 10:19:54 +1100

    added tests for letter class

--- test/letter.test.js
+++ test/letter.test.js
@@ -1,19 +1,24 @@
+const Letter = require("../lib/letter")
 
-const Letter = require("../lib/letter");
-
-describe("Letter class", ()=>{
-    it("characters that are not digits or letters are isntantly visible",()=>{
-        expect(new Letter("@").visible).toBe(true)
+describe ("Letter Class", ()=>{
+    it ("characters that are not digital or letters are instantly visible", ()=>{
+        expect(new Letter("?").visible).toBe(true)
     })
 
     it("toString returns _ for letters", ()=>{
-        expect (new Letter("a").toString()).toBe("_")
+        expect(new Letter('a').toString()).toBe("_")
+    })
+    
+    it ("returns guess correct as true", ()=>{
+        expect(new Letter('g').guess('g')).toBe(true)
     })
 
-    it("correct guess returns true", ()=>{
-        expect(new Letter("j").guess("j")).toBe(true)
+    it("incorrect guess returns false", ()=>{
+        expect(new Letter('g').guess('y')).toBe(false)
+    })
+    it("get solution",()=>{
+       
+            expect(new Letter("1").getSolution()).toBe("1")
+        
     })
 })
-
-
-
